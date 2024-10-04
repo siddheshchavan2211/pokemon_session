8a697a2db0c7dd500783fbee9a95b7401959fb09
Siddhesh Chavan <149790685+siddheshchavan2211@users.noreply.github.com>
2024-10-04 09:24:06 +0000

    cards render on ui

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,33 @@
+import data from './data.json';
+
 const inputelement = document.querySelector('input');
-document.addEventListener('keypress', function () {
-  console.log('aaaa');
+const rowelement = document.querySelector('#row');
+
+console.log(rowelement);
+for (let objectsdata of data) {
+  const div = document.createElement('div');
+  div.classList.add('col');
+  div.innerHTML = `
+  <div class="card">
+            <img
+              src=${objectsdata.image}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${objectsdata.name}</h5>
+              <p class="card-text">
+                ${objectsdata.description}
+              </p>
+            </div>
+          </div>`;
+  //   p.textContent = objectsdata.name;
+  rowelement.appendChild(div);
+}
+function renderpokemons(list) {}
+document.addEventListener('keypress', function (event) {
+  if (event.key === '/') {
+    event.preventDefault();
+    inputelement.focus();
+  }
 });
